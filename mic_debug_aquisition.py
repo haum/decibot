cad387d020712525214100d61b70de86223cc03f
JackDesBwa <jackdesbwa@gmail.com>
2026-07-05 18:46:17 +0200

    Add script to get sound data for debug

---
+++ mic_debug_aquisition.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import argparse
+import socket
+import wave
+import requests
+import struct
+
+def debug_mic_stream(ip, local_ip, local_port=9000):
+    requests.get(f"http://{ip}/debug_mic_addr/{local_ip}:{local_port}", timeout=3)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((local_ip, local_port))
+    sock.settimeout(1.0)
+
+    try:
+        while True:
+            try:
+                data, addr = sock.recvfrom(4096)
+                values = [v[0] for v in struct.iter_unpack("<i", data)]
+                left = values[0::2]
+                right = values[1::2]
+                yield left, right
+            except socket.timeout:
+                pass
+    except KeyboardInterrupt:
+        pass
+    finally:
+        sock.close()
+        requests.get(f"http://{ip}/debug_mic_addr/None", timeout=3)
+
+def main():
+    parser = argparse.ArgumentParser(description="Get Decibot microphone data for debug")
+    parser.add_argument("--ip", required=True, help="Remote IP")
+    parser.add_argument("--local-ip", required=True, help="Local IP")
+    parser.add_argument("--local-port", type=int, default=9000, help="Local UDP port")
+    parser.add_argument("--file", default="capture.wav", help="File to write")
+    args = parser.parse_args()
+
+    wav = wave.open(args.file, "wb")
+    wav.setnchannels(2)
+    wav.setsampwidth(4)
+    wav.setframerate(32000)
+
+    for left, right in debug_mic_stream(args.ip, args.local_ip, args.local_port):
+        b = b"".join(struct.pack("<i", l) + struct.pack("<i", r) for l, r in zip(left, right))
+        wav.writeframes(b)
+
+    wav.close()
+
+if __name__ == "__main__":
+    main()
